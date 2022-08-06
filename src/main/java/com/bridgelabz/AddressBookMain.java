127a83e5e6e773c75543aad0df98bb10d580fcf2
Nagraj94 <ngowda335@gmail.com>
2022-08-06 17:59:43 +0530

    Ability to add multiple AddressBook

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -7,16 +7,50 @@ public class AddressBookMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         AddressBook contact1 = new AddressBook();
+        AddressBook contact2 = new AddressBook();
+        AddressBook contact3 = new AddressBook();
         System.out.println("1.Add contact\n2.Edit contact\n3.Delete contact\n4.Add multiple contact");
         int check = sc.nextInt();
-        if (check == 1)
-            contact1.createContact();
-        else if (check == 2) {
-            contact1.edit();
+        if (check == 1){
+            System.out.println("Enter addressbook number");
+            int ck = sc.nextInt();
+            if (ck == 1) {
+                contact1.createContact();
+            }else if (ck == 2) {
+                contact2.createContact();
+            } else if (ck == 3) {
+                contact3.createContact();
+            }
+        } else if (check == 2) {
+            System.out.println("Enter addressbook number");
+            int ck = sc.nextInt();
+            if (ck == 1) {
+                contact1.edit();
+            }else if (ck == 2) {
+                contact2.edit();
+            } else if (ck == 3) {
+                contact3.edit();
+            }
         } else if (check == 3) {
-            contact1.delete();
+            System.out.println("Enter addressbook number");
+            int ck = sc.nextInt();
+            if (ck == 1) {
+                contact1.delete();
+            }else if (ck == 2) {
+                contact2.delete();
+            } else if (ck == 3) {
+                contact3.delete();
+            }
         } else if (check == 4) {
-            contact1.addMultipleContact();
+            System.out.println("Enter addressbook number");
+            int ck = sc.nextInt();
+            if (ck == 1) {
+                contact1.addMultipleContact();
+            }else if (ck == 2) {
+                contact2.addMultipleContact();
+            } else if (ck == 3) {
+                contact3.addMultipleContact();
+            }
         }
 
     }
