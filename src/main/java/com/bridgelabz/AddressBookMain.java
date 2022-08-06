59c49b08ecf6db86090a31a971cc327248117114
Nagraj94 <ngowda335@gmail.com>
2022-08-06 17:19:16 +0530

    Ability add new contact to addressbook

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -3,9 +3,10 @@ package com.bridgelabz;
 public class AddressBookMain {
 
     public static void main(String[] args) {
-
         AddressBook contact1 = new AddressBook();
+        AddressBook contact2 = new AddressBook();
         contact1.createContact();
+        contact2.createContact();
 
     }
 }
